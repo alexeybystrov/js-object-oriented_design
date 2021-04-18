004705f89cc9eab5e442998253c388ba03fee4f0
Alexey Bystrov <alexeybystrov@me.com>
2021-04-18 17:06:19 +0300

    finish lesson 5

---
+++ src/lesson5/index.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+
+const genres = [
+  'drama', 'horror', 'fantasy', 'classic',
+];
+
+export default (books) => {
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    author: yup.string().required(),
+    pagesCount: yup.number(),
+    link: yup.string().min(1).url(),
+    genre: yup.string().oneOf(genres),
+  });
+
+  return books.filter((book) => !schema.isValidSync(book));
+};
